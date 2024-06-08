40cf5e4093ca4eb2d8469dfacc617d9a31010c20
anjalykrishna522002 <anjalykrishnaas@gmail.com>
2024-06-08 11:13:25 +0530

    done

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -10,11 +10,15 @@ const AddAlbum = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                 <div className="row g-3">
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <label htmlFor="" className="form-label">Album Title</label>
+                        <label htmlFor="" className="form-label">User Id</label>
                         <input type="text" className="form-control" />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <label htmlFor="" className="form-label">Album Code</label>
+                        <label htmlFor="" className="form-label">Id</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Title</label>
                         <input type="text" className="form-control" />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
@@ -49,10 +53,7 @@ const AddAlbum = () => {
                         <label htmlFor="" className="form-label">Price</label>
                         <input type="text" className="form-control" />
                     </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                        <label htmlFor="" className="form-label">Edition</label>
-                        <input type="text" className="form-control" />
-                    </div>
+                    
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <button className="btn btn-success">Submit</button>
                     </div>
